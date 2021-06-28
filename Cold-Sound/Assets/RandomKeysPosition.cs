577a954d2b817549c13c642eb66fc55d5c742c72
Mura <m.colin@ludus-academie.com>
2021-06-28 17:03:24 +0200

    Spaw keys random

---
+++ Cold-Sound/Assets/RandomKeysPosition.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class RandomKeysPosition : MonoBehaviour
+{
+    [SerializeField] List<GameObject> keysPrefab = new List<GameObject>();
+    List<GameObject> keysInstances = new List<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            ChoseLocation(i);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChoseLocation(int indexChild)
+    {
+        int index = Random.Range(0, 3);
+        Vector3 pos = transform.GetChild(indexChild).GetChild(index).transform.position;
+
+        keysInstances.Add(Instantiate(keysPrefab[indexChild], pos, Quaternion.identity));
+        keysInstances[indexChild].transform.SetParent(transform.GetChild(indexChild).GetChild(index).transform);
+        keysInstances[indexChild].name = "Key " + (indexChild + 1);
+    }
+}

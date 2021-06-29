5f7d536af1954c2b5f5758c4c2262c5e4024ec14
Mura <m.colin@ludus-academie.com>
2021-06-29 15:18:16 +0200

    interactions clés

---
+++ Cold-Sound/Assets/collectKeys.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectKeys : MonoBehaviour
+{
+    private GameManager GM;
+    private KeysPanel_UI KP_UI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = FindObjectOfType<GameManager>();
+        KP_UI = FindObjectOfType<KeysPanel_UI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GM.nbKeys++;
+        KP_UI.AddKeyUI();
+        Destroy(gameObject);
+    }
+}
